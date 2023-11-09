9da8a423314b83d0a9339d523deba55fa8f1bb12
Tibi Halasi <czalbert.tibor04@gmail.com>
2023-11-09 13:09:03 +0100

    Feladat bejefezése

--- WPF_Calculator/MainWindow.xaml.cs
+++ WPF_Calculator/MainWindow.xaml.cs
@@ -27,57 +27,27 @@ namespace WPF_Calculator
 
         private void buttonPlus_Click(object sender, RoutedEventArgs e)
         {
-            string number1String = textboxNumber1.Text;
-            string number2String = textboxNumber2.Text;
-            EllenorzesUres(number1String, number2String);
-            if (Ellenorzes(number1String, number2String) == true)
-            {
-                labelEredmeny.Content = int.Parse(textboxNumber1.Text) + int.Parse(textboxNumber2.Text);
-            }
+            szamokEllenorzese(1);
         }
 
         private void buttonMinus_Click(object sender, RoutedEventArgs e)
         {
-            string number1String = textboxNumber1.Text;
-            string number2String = textboxNumber2.Text;
-            EllenorzesUres(number1String, number2String);
-            if (Ellenorzes(number1String, number2String) == true)
-            {
-                labelEredmeny.Content = int.Parse(textboxNumber1.Text) - int.Parse(textboxNumber2.Text);
-            }
+            szamokEllenorzese(2);
         }
 
         private void buttonSzorzas_Click(object sender, RoutedEventArgs e)
         {
-            string number1String = textboxNumber1.Text;
-            string number2String = textboxNumber2.Text;
-            EllenorzesUres(number1String, number2String);
-            if (Ellenorzes(number1String, number2String) == true)
-            {
-                labelEredmeny.Content = int.Parse(textboxNumber1.Text) * int.Parse(textboxNumber2.Text);
-            }
+            szamokEllenorzese(3);
         }
 
         private void buttonOsztas_Click(object sender, RoutedEventArgs e)
         {
-            string number1String = textboxNumber1.Text;
-            string number2String = textboxNumber2.Text;
-            EllenorzesUres(number1String, number2String);
-            if (Ellenorzes(number1String, number2String) == true)
-            {
-                labelEredmeny.Content = Math.Round((double.Parse(textboxNumber1.Text) / double.Parse(textboxNumber2.Text)),2);
-            }
+            szamokEllenorzese(4);
         }
 
         private void buttonMaradekos_Click(object sender, RoutedEventArgs e)
         {
-            string number1String = textboxNumber1.Text;
-            string number2String = textboxNumber2.Text;
-            EllenorzesUres(number1String, number2String);
-            if (Ellenorzes(number1String, number2String) == true)
-            {
-                labelEredmeny.Content = (int.Parse(textboxNumber1.Text) % int.Parse(textboxNumber2.Text));
-            }
+            szamokEllenorzese(5);
         }
 
         private bool EllenorzesUres(string n1, string n2)
@@ -102,6 +72,7 @@ namespace WPF_Calculator
 
         private bool SzamE(string szam)
         {
+            //int.TryParse(szam)
             try
             {
                 Convert.ToInt32(szam);
@@ -111,6 +82,61 @@ namespace WPF_Calculator
                 return false;
             }
             return true;
+        }
+
+        private void szamokEllenorzese(int number)
+        {
+            string number1String = textboxNumber1.Text;
+            string number2String = textboxNumber2.Text;
+            EllenorzesUres(number1String, number2String);
+            if (Ellenorzes(number1String, number2String) == true)
+            {
+                switch (number)
+                {
+                    case 1:
+                        labelEredmeny.Content = OsszeAdas(int.Parse(textboxNumber1.Text), int.Parse(textboxNumber2.Text));
+                    break;
+                    case 2:
+                        labelEredmeny.Content = Kivonas(int.Parse(textboxNumber1.Text), int.Parse(textboxNumber2.Text));
+                        break;
+                    case 3:
+                        labelEredmeny.Content = Szorzas(int.Parse(textboxNumber1.Text), int.Parse(textboxNumber2.Text));
+                        break;
+                    case 4:
+                        labelEredmeny.Content = Osztas(double.Parse(textboxNumber1.Text), double.Parse(textboxNumber2.Text));
+                        break;
+                    case 5:
+                        labelEredmeny.Content = Maradekos(int.Parse(textboxNumber1.Text), int.Parse(textboxNumber2.Text));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private int OsszeAdas(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        private int Kivonas(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+
+        private int Szorzas(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+
+        private double Osztas(double n1, double n2)
+        {
+            return Math.Round((n1 / n2),2);
+        }
+
+        private int Maradekos(int n1, int n2)
+        {
+            return n1 % n2;
         }
 
     }
